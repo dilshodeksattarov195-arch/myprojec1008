af5d1ab2c541af1c65fa5b243bb47d540f7dc969
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 15:18:25 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class invoiceUalculateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = invoiceUalculateWorker()
+    print("Worker engine initialized.")
